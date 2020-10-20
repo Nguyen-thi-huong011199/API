def6aa09154df74f34012578a24498cdad14a9b0
Nguyen-thi-huong011199 <70245104+Nguyen-thi-huong011199@users.noreply.github.com>
2020-10-20 09:54:24 +0700

    update

---
+++ Doan5/Models/Hoso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Doan5.Models
+{
+    public partial class Hoso
+    {
+        public int MaHs { get; set; }
+        public int? MaCv { get; set; }
+        public int? Mauser { get; set; }
+        public int? MaKv { get; set; }
+        public string Email { get; set; }
+        public string Trangthai { get; set; }
+        public string FileHs { get; set; }
+
+        public virtual Vieclam MaCvNavigation { get; set; }
+        public virtual Khuvuc MaKvNavigation { get; set; }
+        public virtual UserNd MauserNavigation { get; set; }
+    }
+}
